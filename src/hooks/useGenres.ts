c7fe9dd88de963899acfdf6c9c78a54de0b0ac92
rektgit2024 <myemailispatrick@gmail.com>
2024-04-04 15:12:00 +0800

    refactor - create lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
+
 import APIClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
+import genres from "../data/genres";
 
 const apiClient = new APIClient<Genre>('/genres');
 
@@ -15,7 +16,7 @@ const useGenres = () => useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,    //24h    
-    //initialData: {count: genres.length, results: genres} //data inserted into the cache
+    initialData: genres //data inserted into the cache
 });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,9 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient, { FetchResponse } from "../services/api-client";
-
+import APIClient from "../services/api-client";
+import platforms from "../data/platforms";
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
-
 
 export interface Platform {
     id: number;
@@ -15,7 +14,7 @@ const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h
-
+    initialData: platforms
 })
 
 export default usePlatforms;
